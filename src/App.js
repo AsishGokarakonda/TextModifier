357c2eaef58375e12bbb0f97c86f1b65d51d0bad
Asish <sridharnagamani5064@gmail.com>
2022-04-27 12:56:49 +0530

    Alerts,Dark mode added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,47 @@ import './App.css';
 import Navbar from './components/Navbar.js'
 import About from './components/About';
 import Form from './components/Form';
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState("light")
+  const [alert, setAlert] = useState(null)
+  const promptAlert =(message,type)=>{
+    setAlert({
+      message:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+  const toggleMode = () =>{
+    if (mode === "light"){
+      setMode("dark")
+      document.body.style.backgroundColor = "black"
+      document.body.style.color = "white"
+      promptAlert("Dark has been enabled","light")
+    }
+    else{
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+      document.body.style.color = "black"
+      promptAlert("Light mode has been enabled","dark")
+    }
+  }
   return (
     <>
       <Router>
-        <Navbar title="TextModifier" />
+        <Navbar title="TextModifier" mode={mode} toggle={toggleMode}/>
+        <Alert alert={alert}/>
         <Routes>
-          <Route exact path = "/" element={<Form toptext="Enter the text in below text area"/>}/>
+          <Route exact path = "/" element={<Form toptext="Enter the text in below text area" mode={mode} promptAlert={promptAlert}/>}/>
         </Routes>
         <Routes>
           <Route exact path = "/about" element={<About/>}/>
--- src/components/Form.js
+++ src/components/Form.js
@@ -11,36 +11,59 @@ export default function Form(props) {
     const clickedUpper = () => {
         let result = text.toUpperCase();
         setText(result)
+        if (text.trim()!==""){
+            props.promptAlert("Text converted to uppercase successfully","primary")
+        }
+        else{
+            props.promptAlert("Please write atleast one alphabet to convert to uppercase","danger")
+        }
     }
     const clickedLower = () => {
         let result = text.toLowerCase();
         setText(result)
+        if (text.trim()!==""){
+            props.promptAlert("Text converted to lowercase successfully","warning")
+        }
+        else{
+            props.promptAlert("Please write atleast one alphabet to convert to lowercase","danger")
+        }
     }
     const clickedDelete = () => {
         document.getElementById("exampleFormControlTextarea1").value = "";
+        setText("")
+        if (text===""){
+            props.promptAlert("Nothing is there to delete","danger")
+        }
+        else{
+            props.promptAlert("Text deleted successfully","danger")
+        }
     }
     const clickedCopy = async () => {
         console.log(text)
         await navigator.clipboard.writeText(text);
         if (text === "") {
-            alert("Nothing is there to copy")
+            props.promptAlert("Nothing is there to copy","danger")
         }
         else {
-            alert('Text copied');
+            props.promptAlert("Text copied successfully","success")
         }
     }
     function WordCount(str) {
         return str.split(' ')
-            .filter(function (n) { return n != '' })
+            .filter(function (n) { return n !== '' })
             .length;
     }
-
+    const mystyle = {
+        color: props.mode === "light"? '#212529':"white",
+        backgroundColor: props.mode === "light"? "white":"#212529",
+      };
+    
     return (
         <>
-            <div className="container">
+            <div className="container" >
                 <div className="mb-3" style={{ marginTop: "30px" }}>
                     <h2>{props.toptext}</h2>
-                    <textarea className="form-control" value={text} id="exampleFormControlTextarea1" onChange={textChanger} onFocus={handleFocus} rows="5"></textarea>
+                    <textarea className="form-control" style={mystyle} value={text} id="exampleFormControlTextarea1" onChange={textChanger} onFocus={handleFocus} rows="5"></textarea>
                 </div>
                 <button type="button " className="btn btn-primary mx-2" onClick={clickedUpper}>Convert to uppercase</button>
                 <button type="button " className="btn btn-warning mx-2" onClick={clickedLower}>Convert to lowercase</button>
